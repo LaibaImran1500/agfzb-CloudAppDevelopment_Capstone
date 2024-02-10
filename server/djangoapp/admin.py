c2b59fa749962ac789628bfe9f28a5863d8d0ba1
LaibaImran1500 <157962750+LaibaImran1500@users.noreply.github.com>
2024-02-10 19:28:49 +0500

    Update admin.py

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,9 +1,4 @@
 from django.contrib import admin
-from django.contrib import admin
-from .models import CarMake, CarModel
-
-
-from django.contrib import admin
 from .models import CarMake, CarModel
 
 class CarModelInline(admin.TabularInline):
