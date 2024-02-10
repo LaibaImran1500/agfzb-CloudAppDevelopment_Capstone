c26e61daaef9caa5b887a2688335a63c99b9c882
LaibaImran1500 <157962750+LaibaImran1500@users.noreply.github.com>
2024-02-10 18:54:19 +0500

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django import forms
 from django.utils.timezone import now
+
 
 # Create your models here.
 
@@ -26,7 +28,52 @@ class CarModel(models.Model):
         return f"{self.year} {self.car_make.name} {self.name}"
 
 
-# <HINT> Create a plain Python class `CarDealer` to hold dealer data
+class CarDealer:
 
+    def __init__(self, _id, _rev, id, city, state, st, address, zip, lat, long, full_name, short_name):
+        # Dealer id
+        self._id = _id
+        # Dealer rev
+        self._rev = _rev
+        # Dealer id
+        self.id = id
+        # Dealer city
+        self.city = city
+        # Dealer state
+        self.state = state
+        # Dealer state
+        self.st = st
+        # Dealer address
+        self.address = address
+        # Dealer zip
+        self.zip = zip
+        # Location lat
+        self.lat = lat
+        # Location long
+        self.long = long
+        # Dealer Full Name
+        self.full_name = full_name
+        # Dealer short name
+        self.short_name = short_name
+
+
+    def __str__(self):
+        return "Dealer name: " + self.full_name
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
+class DealerReview:
+
+    def __init__(self, dealership, name, purchase, review, purchase_date, car_make, car_model, car_year, sentiment, id):
+        self.dealership = dealership
+        self.name = name
+        self.purchase = purchase
+        self.review = review
+        self.purchase_date = purchase_date
+        self.car_make = car_make
+        self.car_model = car_model
+        self.car_year = car_year
+        self.sentiment = sentiment
+        self.id = id
+
+    def __str__(self):
+        return "Review id: " + self.id
