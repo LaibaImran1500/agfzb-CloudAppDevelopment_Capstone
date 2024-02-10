db8f24ea7d9c98ac5678344c144bff430e68489b
LaibaImran1500 <157962750+LaibaImran1500@users.noreply.github.com>
2024-02-10 18:56:17 +0500

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -14,15 +14,15 @@ urlpatterns = [
 
     path(route='contact', view=views.contact, name='contact'),
 
-    path('registration/', views.registration_request, name='registration'),
+    path(route='registration/', view=views.registration_request, name='registration'),
     path(route='login', view=views.login_request, name='login'),
-    path('login/', views.login_request, name='login'),
-    path('logout/', views.logout_request, name='logout'),
+    path(route='login/', view=views.login_request, name='login'),
+    path(route='logout/', view=views.logout_request, name='logout'),
     path(route='index', view=views.get_dealerships, name='index'),
     path(route='', view=views.get_dealerships, name='index'),
     path('admin/', admin.site.urls),
-    path('dealer/<int:dealer_id>/', views.get_dealer_details, name='dealer_details'),
-    path('dealer/<int:dealer_id>/add_review/', views.add_review, name='add_review'),
+    path(route='dealer/<int:dealer_id>/', view=views.get_dealer_details, name='dealer_details'),
+    path(route='dealer/<int:dealer_id>/add_review/', view=views.add_review, name='add_review'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)\
  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
